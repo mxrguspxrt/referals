106396a63ec432238c6c2f0210d8256bcac2ae2f
Margus Pärt <margus@wave.ee>
2018-02-04 12:52:06 +0200

    Started with routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
 import Header from 'components/Header'
 
-import logo from 'images/logo.svg'
-import 'styles/App.css'
+import logo from 'assets/images/logo.svg'
+import 'assets/styles/App.css'
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import reducers from 'reducers/index.js'
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <Header />
-      </div>
+      <Provider store={store}>
+        <Router history={history}>
+          <div className="app">
+            <Header />
+            <Route path="/" component={IndexPage}>
+              <Route path="login" component={LoginPage} />
+              <Route path="profile" component={ProfilePage} />
+              <Route path="profile" component={ProfilePage} />
+              <Route path="invite" component={InvitePage} />
+            </Route>
+          </div>
+        </Router>
+       </Provider>
     )
   }
 
