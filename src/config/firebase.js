43052edb78bc1eaf0be736430df8aaf1a1a24ab2
Margus Pärt <margus@wave.ee>
2018-02-04 11:53:39 +0200

    Absolute imports support

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPQdDrQq6BhC7SzQ9DSlIDZmToZKqWR0w",
+  authDomain: "referalsyo.firebaseapp.com",
+  databaseURL: "https://referalsyo.firebaseio.com",
+  projectId: "referalsyo",
+  storageBucket: "referalsyo.appspot.com",
+  messagingSenderId: "183768368168"
+}
+
+export default firebaseConfig
--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import * as firebase from 'firebase'
+import firebaseConfig from './config/firebase'
 
-const config = {
-  apiKey: "AIzaSyCPQdDrQq6BhC7SzQ9DSlIDZmToZKqWR0w",
-  authDomain: "referalsyo.firebaseapp.com",
-  databaseURL: "https://referalsyo.firebaseio.com",
-  projectId: "referalsyo",
-  storageBucket: "referalsyo.appspot.com",
-  messagingSenderId: "183768368168"
-}
-firebase.initializeApp(config)
+firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
