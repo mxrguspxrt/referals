10843cbe8e64feb64b441e734d5916316af6a36f
Margus Pärt <margus@wave.ee>
2018-02-05 04:43:19 +0200

    save

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
 
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
-exports.createUserWithDefaultReferal = functions.auth.user().onCreate(event => {
-  const user = event.data.user;
-  console.log(user)
-});
+// const admin = require('firebase-admin');
+// admin.initializeApp(functions.config().firebase);
+//
+// exports.createUserWithDefaultReferal = functions.auth.user().onCreate(event => {
+//   const user = event.data.user;
+//   console.log(user)
+// });
